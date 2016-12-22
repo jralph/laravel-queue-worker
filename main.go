7c8d19c14724d543d5f0810bebe839ce62bd8dcb
Joseph Ralph <joseph@pmconnect.co.uk>
2016-12-22 16:24:53 +0000

    Tidying code and adding comments.

--- main.go
+++ main.go
@@ -27,10 +27,33 @@ func main() {
 
 	getopt.Parse()
 
-	runCommands(*artisanPath, *numberOfProcesses, *queue, *delay, *memory, *sleep, *timeout, *tries, *staggered)
+	runCommands(
+		*artisanPath,
+		*numberOfProcesses,
+		*queue,
+		*delay,
+		*memory,
+		*sleep,
+		*timeout,
+		*tries,
+		*staggered,
+	)
 }
 
-func runCommands(artisanPath string, numProcs int, queue string, delay int, memory int, sleep int, timeout int, tries int, staggered bool) {
+/**
+ * Handle the asynchronous running of the worker commands.
+ */
+func runCommands(
+	artisanPath string,
+	numProcs int,
+	queue string,
+	delay int,
+	memory int,
+	sleep int,
+	timeout int,
+	tries int,
+	staggered bool,
+) {
 	fmt.Println(" ==> Starting processes...")
 
 	var waitGroup sync.WaitGroup
@@ -38,13 +61,41 @@ func runCommands(artisanPath string, numProcs int, queue string, delay int, memo
 	waitGroup.Add(numProcs)
 
 	for i := 0; i < numProcs; i++ {
-		go runCommand(i, waitGroup, artisanPath, queue, delay, memory, sleep, timeout, tries, staggered)
+		go runCommand(
+			i,
+			waitGroup,
+			artisanPath,
+			queue,
+			delay,
+			memory,
+			sleep,
+			timeout,
+			tries,
+			staggered,
+		)
 	}
 
 	waitGroup.Wait()
 }
 
-func runCommand(id int, waitGroup sync.WaitGroup, artisanPath string, queue string, delay int, memory int, sleep int, timeout int, tries int, staggered bool) {
+/**
+ * Handle the running of an artisan queue worker and
+ * handle restarting and error logging.
+ */
+func runCommand(
+	id int,
+	waitGroup sync.WaitGroup,
+	artisanPath string,
+	queue string,
+	delay int,
+	memory int,
+	sleep int,
+	timeout int,
+	tries int,
+	staggered bool,
+) {
+	// If we have requested to stagger the process spawning
+	// sleep for a random amount of time between 0 and 5 seconds.
 	if staggered {
 		time.Sleep(time.Duration(rand.Intn(5)) * time.Second)
 	}
@@ -61,21 +112,46 @@ func runCommand(id int, waitGroup sync.WaitGroup, artisanPath string, queue stri
 		fmt.Sprintf("--tries=%d", tries),
 	)
 
-	fmt.Println(fmt.Sprintf("\033[36m ==>\033[37m Starting process\033[32m [%d]\033[33m [%s]\033[37m", id, strings.Join(command.Args, " ")))
+	fmt.Println(
+		fmt.Sprintf(
+			"\033[36m ==>\033[37m Starting process\033[32m [%d]\033[33m [%s]\033[37m",
+			id,
+			strings.Join(command.Args, " "),
+		),
+	)
 
 	command.Stdout = os.Stdout
 	command.Stderr = os.Stderr
 
 	if err := command.Run(); err != nil {
+		// If the command has exited with an error
+		// do not restart it automatically.
 		defer waitGroup.Done()
 
 		printError(errors.New(fmt.Sprintf("Process %d stopped due to an error.", id)))
 	} else {
+		// If the command has exited with success
+		// restart the command.
 		fmt.Printf("\033[31m ==> Process [%d] stopped, possibly due to queue restart signal. Restarting...\033[37m\n", id)
-		runCommand(id, waitGroup, artisanPath, queue, delay, memory, sleep, timeout, tries, staggered)
+
+		runCommand(
+			id,
+			waitGroup,
+			artisanPath,
+			queue,
+			delay,
+			memory,
+			sleep,
+			timeout,
+			tries,
+			staggered,
+		)
 	}
 }
 
+/**
+ * Print out a given error.
+ */
 func printError(err error) {
-	fmt.Println(fmt.Sprintf("\033[31m ==> Error: %s \033[37m\n", err.Error()))
+	fmt.Println(fmt.Sprintf("\033[31m ==> Error: %s \033[37m", err.Error()))
 }
